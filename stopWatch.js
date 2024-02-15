576b3fc0c31f7318fa33af1f375259565a9a87f2
BrennanMalone <blmalone1014@gmail.com>
2024-02-15 10:02:57 -0600

    Features List

--- stopWatch.js
+++ stopWatch.js
@@ -1,1 +1,5 @@
-
+//- Features we should have -
+//Buttons: Start, Pause, Reset, Lap 
+//Convert our time into hours:minutes:seconds ~ maybe days?
+//Something to keep track of our lap times ~ maybe something to keep track of best/worst lap
+//Expanding UI, so things like laps and bigger time periods only show up when they need to
