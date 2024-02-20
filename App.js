d11cae7a76a767ed30e11567df1685bc5e5fc58b
Brennan Malone <51138161+FalleyMalone@users.noreply.github.com>
2024-02-20 01:10:18 -0600

    Add files via upload
    Finished and Working

---
+++ App.js
@@ -0,0 +1,23 @@
+//- Features we should have -
+//Buttons: Start, Pause, Reset, Lap
+//Convert our time into hours:minutes:seconds ~ maybe days?
+//Something to keep track of our lap times ~ maybe something to keep track of best/worst lap
+//Expanding UI, so things like laps and bigger time periods only show up when they need to
+
+import StopWatch from './components/StopWatch';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StopWatch/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
